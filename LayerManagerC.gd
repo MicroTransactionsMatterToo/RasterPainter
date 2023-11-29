15b63c71ee8786e48ab055438e22080537b7b487
= <=>
2023-11-29 14:24:30 +1300

    LayerManager rewrite complete

--- LayerManagerC.gd
+++ LayerManagerC.gd
@@ -4,13 +4,10 @@ var script_class = "tool"
 # Principal class in charge of retreiving and creating ShadowLayers
 # Does not hold state other than loaded layers
 class LayerManager extends Object:
-    var _layers = []
-    var loaded_layers setget ,_get_loaded_layers
-
-    signal layer_created(layer)
+    var loaded_layers := {}
+    
 
     var Global
-
     var ShadowLayerC
     var ShadowLayer
 
@@ -18,35 +15,40 @@ class LayerManager extends Object:
 
     func logv(msg):
         if LOG_LEVEL > 3:
-            printraw("[V] LM: ")
+            printraw("[V] <LayerManager>: ")
             print(msg)
         else:
             pass
 
+    func logd(msg):
+        if LOG_LEVEL > 2:
+            printraw("[D] <LayerManager>: ")
+            print(msg)
+        else:
+            pass
+    
     func logi(msg):
         if LOG_LEVEL >= 1:
-            printraw("[I] LM: ")
+            printraw("[I] <LayerManager>: ")
             print(msg)
         else:
             pass
 
     func _init(global).() -> void:
         self.Global = global
+        # Load classes
         ShadowLayerC =	ResourceLoader.load(Global.Root + "ShadowLayerC.gd", "GDScript", true)
         ShadowLayer = 	load(Global.Root + "ShadowLayerC.gd").ShadowLayer
 
         self.name = "LayerManager"
-
-    # Frees loaded layers and then itself
+    
     func cleanup() -> void:
-        for layer in self.loaded_layers:
+        logv("Cleanup called, freeing layers")
+        for layer in self.loaded_layers.values():
             if is_instance_valid(layer):
                 layer.queue_free()
+        logv("Layers freed, freeing self")
         self.free()
-    
-    # getter
-    func _get_loaded_layers() -> Array:
-        return self._layers
 
     # Utility function that splits key from Global.World.EmbeddedTextures into it's parts
     func get_key_info(key: String):
@@ -57,139 +59,126 @@ class LayerManager extends Object:
         else:
             return {
                 "level": int(split_key[0]),
-                "layer": int(split_key[1]),
+                "z_index": int(split_key[1]),
                 "modulate": split_key[2],
                 "name": split_key[3],
                 "uuid": split_key[4]
             }
-
-    # Creates a new layer and returns it, or returns the existing layer matching
-    # the parameters
-    func create_layer(level_id, layer_num, lname = "New Layer"):
-        logv("Attempting to create layer at [{lvl}, {lyr}]".format({
-            "lvl": level_id,
-            "lyr": layer_num
-        }))
-        # First try and load the layer, to avoid creating collisions
-        var rlayer = self.load_layer(level_id, layer_num)
-
-        if rlayer != null:
-            logv("Aborting creation, layer already exists: " + str(rlayer))
-            return rlayer
-
-        
-        rlayer = ShadowLayer.new(Global.World)
-        rlayer.create_new(level_id, layer_num, lname)
-        logv("Created layer: " + str(rlayer))
-        self._layers.append(rlayer)
-
-        self.emit_signal("layer_created", rlayer)
-
-        return rlayer
-
-
-    # Finds all existing layers for the given level ID, and then
-    # loads those not already loaded.
-    func load_level_layers(level_id) -> Array:
-        logv("Loading layers for level: " + str(level_id))
     
-        var level_layers = []
+    # ===== LAYERS =====
+
+    # Adds the provided layer to loaded_layers
+    func add_layer(new_layer):
+        logv("Adding layer %s to managed layers" % new_layer)
+
+        if new_layer.uuid in self.loaded_layers.keys():
+            logd("Layer %s already managed, ignoring" % new_layer)
+            return false
+        
+        self.loaded_layers[new_layer.uuid] = new_layer
+        return true
+
+    func load_layer(layer_key: String):
+        logv("Loading layer with key: %s" % layer_key)
+        
+        var key_info = self.get_key_info(layer_key)
+        if key_info["uuid"] in self.loaded_layers.keys():
+            logv("Layer already loaded")
+            return self.loaded_layers[key_info["uuid"]]
         
         for key in Global.World.EmbeddedTextures.keys():
-            if Global.World.EmbeddedTextures[key] == null:
-                print("FUCK")
-                continue
-            var key_info = self.get_key_info(key)
-            if int(key_info["level"]) == level_id:
-                var nlayer = self.load_layer(level_id, key_info["layer"], key_info["uuid"])
-                level_layers.append(nlayer)
-        
-        for layer in self.loaded_layers:
-            if !level_layers.has(layer) and layer.level_id == level_id:
-                level_layers.append(layer)
+            var emb_key = self.get_key_info(key)
+            if (
+                emb_key["uuid"] == key_info["uuid"] and
+                Global.World.EmbeddedTextures[key] != null
+            ):
+                logv("Found key in textures, loading")
+                var new_layer = ShadowLayer.new()
+                new_layer.create_from_embedded_key(key)
 
-        level_layers.sort_custom(self, "sort_layers_desc")
+                return new_layer
+        
+        logv("Unable to load key %s" % layer_key)
+        return null
+
+    func get_layers_in_level(level_id):
+        logv("Fetching layers for level %s" % level_id)
+        var level_layers := []
+
+        for key in Global.World.EmbeddedTextures.keys():
+            if Global.World.EmbeddedTextures[key] == null:
+                logv("Ignoring %s, value is null" % key)
+                continue
+            
+            var key_info = self.get_key_info(key)
+            if key_info["level"] == level_id:
+                var new_layer = self.load_layer(key)
+                self.add_layer(new_layer)
+
+        for layer in self.loaded_layers.values():
+            if layer.level_id == level_id:
+                level_layers.append(layer)
 
         return level_layers
 
-
-    # Attempts to load the given layer. 
-    # Returns null if layer does not exist
-    func load_layer(level_id, layer_num, uuid = null):
-        logv("Get layer Level: {lvl}, Layer: {lyr}".format({
-            "lvl": level_id,
-            "lyr": layer_num
-        }))
-
-        # Check to see if we've already loaded the layer
-        for layer in self.loaded_layers:
-            if  (
-                (
-                    layer.level_id == level_id and
-                    layer.layer_num == layer_num
-                ) or (
-                    layer.uuid == uuid and
-                    uuid != null
-                )
-            ):
-                logv("Layer {lyr} already loaded".format({"lyr": str(layer)}))
-                return layer
+    func layer_map(instance: Object, funcname: String, extra_args = []):
+        var map_result := []
+        var map_func = funcref(instance, funcname)
+        if not map_func.is_valid():
+            return null
         
-        # Check to see if it's been previously saved
-        for key in Global.World.EmbeddedTextures.keys():
-            var key_info = self.get_key_info(key)
-            # Check to see if key was valid, to avoid conflicts with future uses
-            # of EmbeddedTexture
-            if key_info == null:
-                continue
-
-            # Ignore keys with a null value
-            if Global.World.EmbeddedTextures[key] == null:
-                print("WAS NULL")
-                continue
-            
-            if (
-                key_info["level"] == level_id   and
-                key_info["layer"] == layer_num
-            ):
-                logv("Loading layer with key: " + str(key))
-                var nlayer = ShadowLayer.new(Global.World)
-                nlayer.create_from_embedded_key(key)
-                self._layers.append(nlayer)
-                return nlayer
+        for layer in self.loaded_layers.values():
+            var result = map_func.call_funcv([layer] + extra_args)
+            map_result.append(result)
         
-        return null
-    
+        return map_result
+
+    func layer_filter(instance: Object, funcname: String, extra_args = []):
+        var filter_result := []
+        var filter_func = funcref(instance, funcname)
+        if not filter_func.is_valid():
+            return null
+        
+        for layer in self.loaded_layers.values():
+            if filter_func.call_funcv([layer] + extra_args):
+                filter_result.append(layer)
+        
+        return filter_result
+
+    func layer_z_indexes(level_id) -> Array:
+        var z_idxs := []
+        for layer in self.layer_filter(self, "lid_lambda", [level_id]):
+            z_idxs.append(layer.z_index)
+
+        z_idxs.sort()
+        z_idxs.invert()
+
+        return z_idxs
+
+    # ====== LEVELS ======
     # Creates an invisible text object with a unique node_id on the given level
-    func create_level_id(level):
-        logv("Generating new level id for " + level.Label)
+    func create_level_id(level: Object):
+        logv("Generated new level ID for %s" %level.Label)
+
         var new_text = level.Texts.CreateText()
         new_text.SetFontColor(Color("#DEADBEEF"))
         new_text.SetFont("Libre Baskerville", 20)
-        # Generate a random number for node_id, between 100000 and 200000
+
         var text_id = 100000 + (randi() % 100000)
         new_text.set_meta("node_id", text_id)
         return text_id
-    
-    # Attempts to find the ID for the current level.
-    # Automatically creates ID if it can't find one
-    func get_level_id(level):
-        var level_ids = self.get_level_ids()
+
+    func get_level_id(level: Object, create_if_missing = true):
+        var level_ids := self.get_level_ids()
+
         for key in level_ids.keys():
             if level == level_ids[key]:
                 return key
         
-        var key = self.create_level_id(level)
-        return key
-        
-    # Gets a level by ID
-    func get_level_by_id(id):
-        for level in Global.World.AllLevels:
-            for textChild in level.Texts.get_children():
-                if textChild.GetNodeID() == id:
-                    return level
+        if create_if_missing:
+            var key = self.create_level_id(level)
+            return key
 
-    # Returns a dict of all levels, mapped as id: Level
     func get_level_ids() -> Dictionary:
         var id_nodes = {}
         for level in Global.World.AllLevels:
@@ -199,31 +188,16 @@ class LayerManager extends Object:
         
         return id_nodes
 
-    # Returns an array of the Z-index of all layers for the given level ID, descending
-    func z_indexes(level_id):
-        var rval = []
-
-        for layer in self.loaded_layers:
-            if layer.level_id == level_id:
-                rval.append(layer.z_index)
-        
-        rval.sort()
-        rval.invert()
-
-        return rval
-    
-    # Function for sorting layers by z_index
-    func sort_layers(asc: bool):
-        if asc:
-            self._layers.sort_custom(LayerManager, "sort_layers_asc")
-    
-    # Sorts layers in ascending order
-    static func sort_layers_asc(a, b):
-        if a.layer_num < b.layer_num:
+    # ====== SORTING ======
+    static func sort_layers_asc(a, b) -> bool:
+        if a.z_index < b.z_index:
             return true
         return false
 
-    static func sort_layers_desc(a, b):
-        if a.layer_num > b.layer_num:
+    static func sort_layers_desc(a, b) -> bool:
+        if a.z_index > b.z_index: 
             return true
         return false
+
+    # == SHIT THAT SHOULD BE LAMBDAS ==
+    func lid_lambda(layer, level_id): return layer.level_id == level_id
